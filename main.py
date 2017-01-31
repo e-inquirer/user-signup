fd9185a34e3eb6ed765090d625c431c6b2069106
e-inquirer <sorour.a@gmail.com>
2017-01-31 16:20:25 -0500

    added request handlers

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
-        def get(self):
+    
+
+    def get(self):
             self.response.write('Hello world!')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/signup', Signup)
+    ('/welcome', Welcome),
 ], debug=True)
