652697085bfa0ab7ba9b5b0677e7c14283104e5d
e-inquirer <sorour.a@gmail.com>
2017-01-31 18:26:43 -0500

    added P/W label & text input, submit button, and signup form

--- main.py
+++ main.py
@@ -3,16 +3,24 @@ import webapp2
 def build_page(textarea_content):
     username_label = "<label>Username: </label>"
     username_input = "<input type='text' name='username' />"
+    pw_label = "<label>Password: </label>"
+    pw_input = "<input type='password' name='password' />"
+
+
+
 
     # submit button
-    submit = "<input type='submit' />"
+    submit_button = "<input type='submit' />"
 
     # signup form
-    ###
+    form = ("<form method='post'>" +
+            username_label + username_input + "<br>" +
+            pw_label + pw_input + "<br>" +
+            submit_button + "<form/")
     
     header = "<h2>Signup</h2>"
 
-    return (header + username_label + username_input)
+    return header + form 
 
 class MainHandler(webapp2.RequestHandler):
 
