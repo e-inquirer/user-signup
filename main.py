74a7ff6b0031aa62fa588bd080767ec59f74fa6c
e-inquirer <sorour.a@gmail.com>
2017-01-31 17:26:44 -0500

    created build_page() function and inserted signup page title

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import webapp2
 
+def build_page(textarea_content):
+
+    header = "<h2>Signup</h2>"
+
+    return header
+
 class MainHandler(webapp2.RequestHandler):
-    
 
     def get(self):
-            self.response.write('Hello world!')
+        content = build_page("")
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/signup', Signup)
-    ('/welcome', Welcome),
+#    ('/signup', Signup),
+#    ('/welcome', Welcome),
 ], debug=True)
