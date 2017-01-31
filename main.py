7d1dc5449ff5a679cac41ea300f274f804f137f6
e-inquirer <sorour.a@gmail.com>
2017-01-31 18:38:55 -0500

    added P/W Verify, Email label & text inputs; appended to form string

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import webapp2
 
 def build_page(textarea_content):
+
+    # form fields
     username_label = "<label>Username: </label>"
     username_input = "<input type='text' name='username' />"
     pw_label = "<label>Password: </label>"
     pw_input = "<input type='password' name='password' />"
-
-
-
+    pwVerify_label = "<label>Verify Password: </label>"
+    pwVerify_input = "<input type='password' name='pw_verify' />"
+    email_label = "<label>Email (optional): </label>"
+    email_input = "<input type='email' name='email' />"
 
     # submit button
     submit_button = "<input type='submit' />"
@@ -16,6 +19,8 @@ def build_page(textarea_content):
     form = ("<form method='post'>" +
             username_label + username_input + "<br>" +
             pw_label + pw_input + "<br>" +
+            pwVerify_label + pwVerify_input + "<br>" +
+            email_label + email_input + "<br>" +            
             submit_button + "<form/")
     
     header = "<h2>Signup</h2>"
