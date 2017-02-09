9f70263eaf4d234cf15d9bbbb4076fcf0c405d97
e-inquirer <sorour.a@gmail.com>
2017-02-08 20:54:47 -0500

    Validation is functional. Initial page render is still buggy -- posts error messages.

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import webapp2
 import re
 
 
+dictionary = {'UN':'', 'PW':'', 'PV':'', 'EM':''}
+# dictionary = {0:'', 1:'', 2:'', 3:''}
+
+
 user_regEx = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
     return username and user_regEx.match(username)
@@ -15,45 +19,39 @@ def valid_email(email):
     return not email or email_regEx.match(email)
 
 
-def build_page(UN, PW, EM, errFlag):
+def build_page(DICT):
 
     # form fields
     username_label = "<label>Username: </label>"
-    username_input = "<input type='text' name='username' />"
+    username_input = "<input type='text' value='{UN}' name='username' />".format(**DICT)
     pw_label = "<label>Password: </label>"
-    pw_input = "<input type='password' name='password'  />"
+    pw_input = "<input type='password' value='' name='password'  />"
     pwVerify_label = "<label>Verify Password: </label>"
-    pwVerify_input = "<input type='password' name='pwVerify'  />"
+    pwVerify_input = "<input type='password' value='' name='pwVerify'  />"
     email_label = "<label>Email (optional): </label>"
-    email_input = "<input type='email' name='email'  />"
+    email_input = "<input type='email' value='{EM}' name='email' />".format(**DICT)
   
     username_html = username_label + username_input + "<br>"
     pw_html = pw_label + pw_input + "<br>" 
     pw_verify_html = pwVerify_label + pwVerify_input + "<br>" 
     email_html = email_label + email_input + "<br>"
 #    form_tag = "<form action=/welcome method='post'>"
-    form_tag = "<form action=/signup method='post'>"
+    form_tag = "<form action=/ method='post'>"
     
-    if errFlag:
-        username_input = "<input type='text' name='username' value='" + str(UN) + "' />"
-        email_input = "<input type='email' name='email' value='" + str(EM) + "' />"
-        if not valid_username(UN):
-            username_html = username_label + username_input + "INVALID USERNAME" + "<br>"           
-            form_tag = "<form action=/signup method='post'>"
-#        else:
-#            form_tag = "<form action=/welcome method='post'>"
+    # if len(DICT) > 0:
+#    if DICT:
+    if not valid_username(DICT['UN']):
+        username_html = username_label + username_input + "INVALID USERNAME" + "<br>"           
 
-        if not valid_password(PW):
-            pw_html = pw_label + pw_input + "INVALID PASSWORD or VERIFICATION DOESN'T MATCH" + "<br>" 
-            form_tag = "<form action=/signup method='post'>"
-#        else:
-#            form_tag = "<form action=/welcome method='post'>"
-        
-        if not valid_email(EM):
-            email_html = email_label + email_input + "INVALID EMAIL" + "<br>"
-            form_tag = "<form action=/signup method='post'>"
-#    else:
-#        form_tag = "<form action=/welcome method='post'>"
+    if not valid_password(DICT['PW']):
+        pw_html = pw_label + pw_input + "INVALID PASSWORD" + "<br>" 
+
+    if DICT['PV'] != DICT['PW']:
+#    if not valid_password(DICT['PW']):
+        pw_verify_html = pwVerify_label + pwVerify_input + "VERIFICATION DOES NOT MATCH" + "<br>"
+    
+    if not valid_email(DICT['EM']):
+        email_html = email_label + email_input + "INVALID EMAIL" + "<br>"
 
     # submit button
     submit_button = "<input type='submit' />"
@@ -73,71 +71,50 @@ def build_page(UN, PW, EM, errFlag):
 class MainHandler(webapp2.RequestHandler):
 
     def get(self):
-        content = build_page("", "", "", False)
+        content = build_page(dictionary)
         self.response.write(content)
 
     def post(self):
-        userName = self.request.get('username')
-        passWord = self.request.get('password')
-        passVerify = self.request.get('pwVerify')
-        eMail = self.request.get('email')
+#        userName = self.request.get('username')
+#        passWord = self.request.get('password')
+#        passVerify = self.request.get('pwVerify')
+#        eMail = self.request.get('email')
+#        dictionary['UN'] = self.request.get('username')
+#        dictionary['PW'] = self.request.get('password')
+#        dictionary['PV'] = self.request.get('pwVerify')
+#        dictionary['EM'] = self.request.get('email')
+        dictionary.update({'UN':self.request.get('username')})
+        dictionary.update({'PW':self.request.get('password')})
+        dictionary.update({'PV':self.request.get('pwVerify')})
+        dictionary.update({'EM':self.request.get('email')})
 
-        if valid_username(userName) and valid_password(passWord) and passWord != passVerify and valid_email(eMail):
-            error_flag = False
+        if valid_username(dictionary['UN']) and valid_password(dictionary['PW']) and dictionary['PW'] == dictionary['PV'] and valid_email(dictionary['EM']):
             self.redirect('/welcome')
+#        elif not valid_username(userName):
+#            case_flag = 1
+#        elif not valid_password(passWord):
+#            case_flag = 2
+#        elif not passWord != passVerify:
+#            case_flag = 3
+#        elif not valid_email(eMail):
+#            case_flag = 4
+
         else:
-            error_flag = True
-            self.redirect('/signup')
-    
-        content = build_page(userName, passWord, eMail, error_flag)
-        self.response.write(content)
-
-
-#
-#        # validation test stub
-#        self.response.write("<p>"+userName+"</p><p>"+passWord+"</p><p>"+passVerify+"</p><p>"+eMail+"</p>")
-############
-
-class Signup(webapp2.RequestHandler):
-
-    def post(self):
-        userName = self.request.get('username')
-        passWord = self.request.get('password')
-        passVerify = self.request.get('pwVerify')
-        eMail = self.request.get('email')
-        
-        if valid_username(userName) and valid_password(passWord) and passWord != passVerify and valid_email(eMail):
-            error_flag = False
-            self.redirect('/welcome')
-        else:
-            error_flag = True
-#            self.redirect('/signup')
-    
-        content = build_page(userName, passWord, eMail, error_flag)
-        self.response.write(content)
-
+            content = build_page(dictionary)
+            self.response.write(content)
 
 class Welcome(webapp2.RequestHandler):
-    # validation test stub
 
-    def post(self):
+    def get(self):
         userName = self.request.get('username')
 #        passWord = self.request.get('password')
 #        passVerify = self.request.get('pwVerify')
 #        eMail = self.request.get('email')
-#
-#        if valid_username(userName) and valid_password(passWord) and passWord != passVerify and valid_email(eMail):
-#            error_flag = False
-#            self.redirect('/welcome')
-#        else:
-#            error_flag = True
-#            self.redirect('/signup')
 
         self.response.write("Welcome, " + userName + "!")
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/signup', Signup),
     ('/welcome', Welcome)
 ], debug=True)
